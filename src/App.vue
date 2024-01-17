7475bfcea11e709d0095e942d4628b0d13e19e05
arisafari.ridwan <arisafari.ridwan@gmail.com>
2024-01-17 22:07:29 +0700

    init apps screen size and bottom navbar

--- src/App.vue
+++ src/App.vue
@@ -1,91 +1,39 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    />
-    <h1 class="text-3xl font-bold underline text-blue-500">Hello world!</h1>
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+  <main class="my-0 mx-auto min-h-full max-w-[360px]">
+    <div
+      class="my-0 mx-auto min-h-screen max-w-480 overflow-x-hidden bg-green-100 pb-[66px]"
+    >
+      <RouterView />
     </div>
-  </header>
 
-  <RouterView />
+    <nav
+      class="fixed bottom-0 z-[2] m-0 flex justify-between items-center w-full max-w-[360px] bg-sky-50 h-[94px] px-3 py-2"
+    >
+      <RouterLink to="/" class="w-[82px] h-[52px] grid place-content-center">
+        <span>icon</span>
+        Home
+      </RouterLink>
+      <RouterLink
+        to="/about"
+        class="w-[82px] h-[52px] grid place-content-center"
+        >Statistic</RouterLink
+      >
+      <RouterLink
+        to="/about"
+        class="w-[82px] h-[52px] grid place-content-center"
+        >History</RouterLink
+      >
+      <RouterLink
+        to="/about"
+        class="w-[82px] h-[52px] grid place-content-center"
+        >Profile</RouterLink
+      >
+    </nav>
+  </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
+<style scoped></style>
