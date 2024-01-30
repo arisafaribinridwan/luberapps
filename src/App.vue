b9eccaa1fa590cbce16e7f640074df3fb052d163
arisafaribinridwan <arisafari.ridwan@gmail.com>
2024-01-30 16:57:43 +0700

    create /welcome page view

--- src/App.vue
+++ src/App.vue
@@ -14,22 +14,12 @@ function greet() {
   <main
     class="my-0 mx-auto min-h-screen max-w-[360px] bg-blue50 flex justify-center items-center"
   >
-    <img src="./assets/icons/logo-splashscreen.svg" alt="Logo Lunas Bersama" />
-    <button
-      @click="greet"
-      class="px-4 py-3 gap-2 absolute bottom-1/4 flex text-white bg-primary w-[320px] rounded-lg justify-center"
-    >
-      Get Started
-      <span>
-        <img src="./assets/icons/icon-arrow-right.svg" />
-      </span>
-    </button>
+    <RouterView />
 
     <!-- <div
       class="my-0 mx-auto min-h-screen max-w-480 overflow-x-hidden bg-background pb-[66px]"
       >
     </div>
-    <RouterView />
 
     <nav
       class="fixed bottom-0 z-[2] m-0 flex justify-between items-center w-full max-w-[360px] bg-blue50 h-[94px] px-3 py-2"
